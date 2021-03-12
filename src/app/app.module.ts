4604dcd541fbbb93ec3655e49ea186ba574b98c9
Phantom-Code <sourabhkanwade10@gmail.com>
2021-03-12 14:15:31 +0530

    hosting on github

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import{FormsModule} from '@angular/forms'
 import { AppComponent } from './app.component';
 import { FilePickerComponent } from './components/file-picker/file-picker.component';
 
@@ -10,7 +10,8 @@ import { FilePickerComponent } from './components/file-picker/file-picker.compon
     FilePickerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/file-picker/file-picker.component.ts
+++ src/app/components/file-picker/file-picker.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
+import { DomSanitizer } from '@angular/platform-browser';
+import { saveAs } from 'file-saver';
+import { Buffer } from 'buffer';
+import * as cryptoJs from 'crypto-js';
 @Component({
   selector: 'app-file-picker',
   templateUrl: './file-picker.component.html',
@@ -7,9 +10,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FilePickerComponent implements OnInit {
   file: any;
-  constructor() {}
+  downloadJsonHref: any;
+  filename: any;
+  password:any;
+  constructor(private sanitizer: DomSanitizer) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    if (localStorage.getItem('theme') === 'theme-dark') {
+      this.setTheme('theme-dark');
+    } else {
+      this.setTheme('theme-light');
+    }
+  }
   readFileAsync(file) {
     return new Promise((resolve, reject) => {
       let reader = new FileReader();
@@ -20,77 +32,68 @@ export class FilePickerComponent implements OnInit {
 
       reader.onerror = reject;
 
-      reader.readAsArrayBuffer(file);
+      reader.readAsText(file);
+    });
+  }
+  readJsonAsync(file) {
+    return new Promise((resolve, reject) => {
+      let reader = new FileReader();
+      reader.onload = () => {
+        let data: any = reader.result;
+        resolve(JSON.parse(data));
+      };
+      reader.onerror = reject;
+      reader.readAsText(file);
+    });
+  }
+  readBlobAsync(file) {
+    return new Promise((resolve, reject) => {
+      let reader = new FileReader();
+      reader.onload = () => {
+        resolve(reader.result);
+      };
+      reader.onerror = reject;
+      reader.readAsText(file);
     });
   }
 
-  async keyGenerator(password, salt) {
-    let enc = new TextEncoder();
-    let keyMaterial = await window.crypto.subtle.importKey(
-      'raw',
-      enc.encode(password),
-      { name: 'PBKDF2' },
-      false,
-      ['deriveBits', 'deriveKey']
-    );
-    return window.crypto.subtle.deriveKey(
-      {
-        name: 'PBKDF2',
-        salt: salt,
-        iterations: 1000,
-        hash: 'SHA-256',
-      },
-      keyMaterial,
-      { name: 'AES-GCM', length: 256 },
-      true,
-      ['encrypt', 'decrypt']
-    );
-  }
-  async readAndEncryptOneFile(file, pass) {
-    let salt = window.crypto.getRandomValues(new Uint8Array(16));
-    let iv = window.crypto.getRandomValues(new Uint8Array(12));
-    let key = await this.keyGenerator(pass, salt);
-    //
-    //
-    let data: any = await this.readFileAsync(file);
+  generateDownloadJsonUri(file) {
+    var theJSON = JSON.stringify(file);
 
-    let cipheredData = await window.crypto.subtle.encrypt(
-      {
-        name: 'AES-GCM',
-        iv,
-      },
-      key,
-      data
+    var uri = this.sanitizer.bypassSecurityTrustUrl(
+      'data:application/octet-stream,' + file
     );
-    console.log(cipheredData);
-
-    return { cipher: cipheredData, salt: salt, iv: iv };
-  }
-  async readAndDecryptOneFile(file, pass) {
-    let data: any = await this.readFileAsync(file);
-    // console.log(data);
-    let decodedString = await String.fromCharCode.apply(
-      null,
-      new ArrayBuffer(data)
-    );
-    let cipher = await JSON.parse(decodedString);
-    console.log(cipher.cipher);
-    let key = await this.keyGenerator(pass, new ArrayBuffer(cipher.salt));
-
-    return {
-      data: await window.crypto.subtle.decrypt(
-        {
-          name: 'AES-GCM',
-          iv: new ArrayBuffer(cipher.iv),
-        },
-        key,
-        new ArrayBuffer(cipher.cipher)
-      ),
-    };
+    this.downloadJsonHref = uri;
   }
   async fileUploadHandler(files) {
-    console.log(files);
-    let x = await this.readAndEncryptOneFile(files[0], 'pssss');
-    console.log(x);
+    let temp: any = await this.readFileAsync(files[0]);
+    console.log(temp, 'temp');
+
+    let name = files[0].name;
+    let ciphertext = cryptoJs.AES.encrypt(temp, this.password).toString();
+    let blob = new Blob([ciphertext], { type: 'application/json' });
+    saveAs(blob, name + '.json');
+  }
+  async fileUploadHandlerDe(files) {
+    let temp: any = await this.readFileAsync(files[0]);
+    let name = files[0].name;
+    name = name.replace('.json', '');
+    let bytes = cryptoJs.AES.decrypt(temp, this.password).toString(
+      cryptoJs.enc.Latin1
+    );
+    console.log(bytes);
+    let blob = new Blob([bytes]);
+    saveAs(blob, name);
+  }
+  setTheme(themeName) {
+    localStorage.setItem('theme', themeName);
+    document.documentElement.className = themeName;
+  }
+  toggleTheme() {
+    if (localStorage.getItem('theme') === 'theme-dark') {
+      this.setTheme('theme-light');
+    } else {
+      this.setTheme('theme-dark');
+    }
   }
 }
